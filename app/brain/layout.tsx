e072bd4c79072565c5e501549143760330421f9b
Håkon <haakonu97@gmail.com>
2024-06-18 12:07:57 +0200

    chore: responsive design update for brain page

--- app/brain/layout.tsx
+++ app/brain/layout.tsx
@@ -10,4 +10,8 @@ export default function BrainLayout(props: {
 const Container = styled.div`
   padding: var(--size-9);
   padding-top: var(--size-5);
+
+  @media screen and (max-width: 768px) {
+    padding: var(--size-6);
+  }
 `;
